2eabcf9b5e6c1730a05c24791a40f83eb04cbe56
Matias <matidae@gmail.com>
2025-06-09 14:57:31 +0200

    reorder github pages, add varcall script, upload  index.html

--- 03_dedup/01_map_stats.sh
+++ 03_dedup/01_map_stats.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 #----------------------------------------------------------------------
-# Run multiple program to get alignment stats
+# Run multiple programs to get alignment stats
+# The final ouptut will be used by 02_map_dedup_report.ipynb for an html report and plots
 # Run: ./01_map_stats.sh
+# Out: summary_table with stats of mapping and deduplication
 #----------------------------------------------------------------------
 out="../../results/03_dedup"
 
-#Run multiple samtools alignment metrics tools
+# Run multiple samtools alignment metrics tools
 conda activate extra
 for i in $(ls ../../data/03_dedup/*.sort.bam | tail -n +34); do 
     prefix=$(basename $i .dedup.sort.bam );    
@@ -16,7 +18,7 @@ for i in $(ls ../../data/03_dedup/*.sort.bam | tail -n +34); do
     mosdepth --by 500000 -t 30  $out"/mosdepth/"$prefix.mosdepth $i
 done
 
-#Run picard CollectInsertSizeMetrics to obtain the histogram plot 
+# Run picard CollectInsertSizeMetrics to obtain the histogram plot 
 conda activate bio
 find ../../data/03_dedup -name "*.sort.bam" | \
     parallel -j 5 'picard CollectInsertSizeMetrics \
@@ -31,25 +33,39 @@ for i in ../../results/03_dedup/insertion_size_metrics/*.pdf;  do
 done
 
 
-#Calculate the exact mean from mosdepth per base output
+# Calculate the exact mean from mosdepth per base output
 ls ../../results/03_dedup/mosdepth/*.mosdepth.per-base.bed.gz | \
     parallel -j 40 'prefix=$(basename {} .mosdepth.per-base.bed.gz); \
     depth=$(zcat {} | awk "{sum+=\$4; count++} END {printf \"%.1f\", sum/count}"); \
     echo -e "$prefix\t$depth"' > ../../results/03_dedup/mosdepth/exact_mean_coverage
 
-#Prepare the tables
-awk 'OFS="\t" {print $1,$2, $4, $5, $3, $6, $15, $7}'  ../../results/01_proc_reads/all_poolseq_report.tsv | \
-    grep -v Idn > ../../results/03_dedup/half_previous_table
-
+# Sort the data by prefix to keep order of the rows
 for i in $(cat ../../results/01_proc_reads/all_poolseq_report.tsv | grep -v Idn | cut -f1); do 
     grep -w $i ../../results/03_dedup/mosdepth/exact_mean_coverage | awk '{print $2}' ; 
 done > ../../results/03_dedup/mosdepth/exact_mean_coverage.sort
 
+# Get a list of how many reads are mapping
 for i in $(cat ../../results/01_proc_reads/all_poolseq_report.tsv | grep -v Idn | cut -f1); do  
     awk '{printf "%.1f\n" , $1/1e6}'  ../../results/03_dedup/samtools_metrics/$i.samtools.before_dedup;  
-done > ../../results/03_dedup/samtools_metrics/nreads_before_dedup_all
+done > ../../results/03_dedup/samtools_metrics/mapped_reads_before_dedup
 
+# Get a list of how many reads remain mapped after deduplication
 for i in $(cat ../../results/01_proc_reads/all_poolseq_report.tsv | grep -v Idn | cut -f1); do 
     cat ../../results/03_dedup/samtools_metrics/$i.samtools.flagstat | grep "mapped.*%" | \
-    sed 's/% : N\/A)//; s/ + 0 mapped (/ /' | awk 'OFS="\t" {printf "%.1f\t%.1f\n", $1/1e6, $2}'; 
-    done > ../../results/03_dedup/samtools_metrics/reads_mapped_all
+    awk 'OFS="\t" {printf "%.1f\n", $1/1e6 }'; 
+done > ../../results/03_dedup/samtools_metrics/mapped_reads_after_dedup
+
+# Prepare the table with data from the previous report
+awk 'OFS="\t" {print $1,$2, $4, $5, $3, $6, $15, $7}'  ../../results/01_proc_reads/all_poolseq_report.tsv | \
+    grep -v Idn > ../../results/03_dedup/half_previous_table
+
+# Write column names for summary table
+echo -e "Idn\tYear\tCountry\tRegion\tTime\tRep\tRaw_reads\tQC_reads\tMapped_reads\tDedup_reads\tMean_depth" \
+    > ../../results/03_dedup/summary_table
+
+# Output a summary table with all the data needed for the html report
+paste ../../results/03_dedup/half_previous_table \
+    ../../results/03_dedup/samtools_metrics/mapped_reads_before_dedup \
+    ../../results/03_dedup/samtools_metrics/mapped_reads_after_dedup  \
+    ../../results/03_dedup/mosdepth/exact_mean_coverage.sort \
+    >> ../../results/03_dedup/summary_table
--- 04_varcalls/00_varcall.sh
+++ 04_varcalls/00_varcall.sh
@@ -1,6 +1,21 @@
+#----------------------------------------------------------------------
+# Runs variant calling in parallel across genomic regions specified in the 'regions_1M' file
+# Uses bcftools mpileup and bcftools call to generate VCF files for each region.
+# Run: ./00_varcall.sh
+# In: ../../data/03_dedup/*.dedup.sort.nam
+# Out: ../../results/04_varcalls/region_{#}_$chunk.vcf
+#----------------------------------------------------------------------
+
 #!/usr/bin/env bash
 
-cat regions_1M | parallel -j40 "region={}; chunk=\$(echo \$region | sed 's/[:-]/_/g'); bcftools mpileup -Ou -I -a FORMAT/AD --max-depth 5000 -r \$region -f ../../data/reference/Ips_typograpgus_LG16corrected.final.fasta ../../data/03
-_dedup/WFIN_Ea_2015.dedup.sort.bam  ../../data/03_dedup/WFIN_Eb_2015.dedup.sort.bam ../../data/03_dedup/WFIN_Ea_2017.dedup.sort.bam  ../../data/03_dedup/WFIN_Eb_2017.dedup.sort.bam ../../data/03_dedup/WFIN_Ea_2020.dedup.sort.bam  ../../dat
-a/03_dedup/WFIN_Eb_2020.dedup.sort.bam ../../data/03_dedup/WFIN_Ea_2022.dedup.sort.bam  ../../data/03_dedup/WFIN_Eb_2022.dedup.sort.bam ../../data/03_dedup/WFIN_Ea_2024.dedup.sort.bam  ../../data/03_dedup/WFIN_Eb_2024.dedup.sort.bam  | bcf
-tools call -vmO v -o ../../results/04_varcalls/WFIN_E_2015.dedup.{#}.\$chunk.vcf"
+BAM_FILES=$(find ../../data/03_dedup/ -name "*.dedup.sort.bam" | sort | tr '\n' ' ')
+
+cat regions_1M | parallel -j40 '
+    region={};
+    chunk=$(echo $region | sed "s/[:-]/_/g");
+    bcftools mpileup -Ou -I -a FORMAT/AD --max-depth 5000 \
+        -r $region \
+        -f ../../data/reference/Ips_typograpgus_LG16corrected.final.fasta \
+        '"$BAM_FILES"' |
+    bcftools call -vmO v -o ../../results/04_varcalls/region_{#}_$chunk.vcf
+'
