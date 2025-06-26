1b39efdbc5ed84333f7764c07475693bee115e28
Matias <matidae@gmail.com>
2025-06-26 12:07:08 +0200

    minor fix, add comments

--- 04_varcalls/00_varcall.sh
+++ 04_varcalls/00_varcall.sh
@@ -1,47 +1,50 @@
 #!/usr/bin/env bash
 
-#----------------------------------------------------------------------
-# Runs variant calling in parallel across genomic regions specified in the 'regions_1M' file
+#--------------------------------------------------------------------------------
+# Runs variant calling in parallel across genomic regions specified regions_1M.txt
 # Uses bcftools mpileup and bcftools call to generate VCF files for each region.
 # Concatenates the chunks into the file ips_merged.vcf.gz
 #
-# Run: ./00_varcall.sh
-# In: ../../data/03_dedup/*.dedup.sort.nam
-# Out: ../../results/04_varcalls/region_{#}_$chunk.vcf and ips_merged.vcf.gz
-#----------------------------------------------------------------------
+# Input: 
+#   - ../../data/03_dedup/*.dedup.sort.bam : mappings of all samples
+# Output: 
+#   - ips_merged.vcf.gz : vcf file for all samples
+#-------------------------------------------------------------------------------
 
 eval "$(conda shell.bash hook)"
 conda activate extra
 
-wd="../../results/04_varcalls"
+work_dir="../../results/04_varcalls"
 genome="../../data/reference"
 bam=$(find ../../data/03_dedup/ -name "*.dedup.sort.bam" | sort | tr '\n' ' ')
 
+bedtools makewindows -g "$genome/Ips_typograpgus_LG16corrected.final.fasta.fai" -w 1000000 \
+    | awk '{print $1":"$2+1"-"$3}' > "$work_dir/regions_1M.txt"
+
 #Run bcftools mpileup and call in parallel taking genome chunks defined in regions_1M
-cat regions_1M | parallel -j40 '
+cat "$work_dir/regions_1M.txt" | parallel -j40 '
     region={};
     chunk=$(echo $region | sed "s/[:-]/_/g");
     bcftools mpileup -Ou -I -a FORMAT/AD --max-depth 5000 \
         -r $region \
         -f '"$genome"/Ips_typograpgus_LG16corrected.final.fasta \
         '"$bam"' |
-    bcftools call -vmO v -o '"$wd"/region_{#}_$chunk.vcf
+    bcftools call -vmO v -o '"$work_dir"/region_{#}_$chunk.vcf
 '
-
 # Compress and index each VCF file
-for i in "$wd"/*.vcf; do
+for i in "$work_dir"/*.vcf; do
     bgzip "$i"
     tabix -p vcf "$i.gz"
 done
 
 # Sort files numerically based on the region number before concatenation
-sorted_vcfs=$(ls "$wd"/region_*.vcf.gz | sort -V)
+sorted_vcfs=$(ls "$work_dir"/region_*.vcf.gz | sort -V)
 
 # Concatenate the sorted VCF files
-bcftools concat -Oz -o "$wd/ips_merged.vcf.gz" $sorted_vcfs
+bcftools concat -Oz -o "$work_dir/ips_merged.vcf.gz" $sorted_vcfs
 
 # Index the merged VCF
-tabix -p vcf "$wd/ips_merged.vcf.gz"
+tabix -p vcf "$work_dir/ips_merged.vcf.gz"
 
-mkdir "$wd/chunks"
-mv "$wd/region_*"  "$wd/chunks/"
+mkdir "$work_dir/chunks"
+mv "$work_dir/region_*"  "$work_dir/chunks/"
--- 04_varcalls/01_filter_vcf.sh
+++ 04_varcalls/01_filter_vcf.sh
@@ -1,56 +1,55 @@
 #!/usr/bin/env bash
 
-#----------------------------------------------------------------------
-# Filter the vcf file by : snps, biallelic, MQ>=20 and QUAL>=20 known genotype and MAF>=0.5
-# Intersects using bedtools with genic and intergenic regions creating two vcf files
-# Get readcount of each of this vcf files to obtain depth per SNP
+#-------------------------------------------------------------------------------
+# Filter the VCF file by : snps, biallelic, MQ>=20 and QUAL>=20 known genotype and MAF>=0.5
+# Intersect VCF with genic and intergenic regions 
+# Extract AD fields from each VCF file
 #
-# Run: ./02_filter_vcf.sh
-# In: ips_merged.vcf
-# Out: Filtered all SNPs vcf.gz file, and alos genic and intergenic SNPs .vcf.gz and readcounts
-#----------------------------------------------------------------------
+# Input:
+#   - ips_merged.vcf: vcf file for all samples
+# Output: 
+#   - genic_readcounts.tsv : read counts of filtered SNPs from genic regions
+#   - intergenic_readcounts.tsv : read counts of filtered SNPs from intergenic regions
+#-------------------------------------------------------------------------------
 
 eval "$(conda shell.bash hook)"
 conda activate extra
 
-wd="../../results/04_varcalls"
+work_dir="../../results/04_varcalls"
 reference="../../data/reference"
 gff="Ips_typograpgus_LG16corrected.liftoff.gff"
-genes="Ips_typograpgus_LG16corrected.liftoff.genes.bed"
+bed="Ips_typograpgus_LG16corrected.liftoff.genes.bed"
 
 # Filter VCF: -v snps (only snps), -m2 -M2 (biallelic) -e 'GT="./."' (exclude missing GT)
-bcftools view "$wd/ips_merged.vcf.gz" -m2 -M2 -v snps -e 'GT="./." || INFO/MQ<20 || QUAL<20' \
-  -Oz -o "$wd/ips.filter.vcf.gz" 
+bcftools view "$work_dir/ips_merged.vcf.gz" -m2 -M2 -v snps -e 'GT="./." || INFO/MQ<20 || QUAL<20' \
+  -Oz -o "$work_dir/ips.filter.vcf.gz" 
 
 # Exclude all SNPs where MAF < 0.05
-bcftools filter "$wd/ips.filter.vcf.gz" -e 'MAF[0] < 0.05' -Oz -o "$wd/ips.filter.m05.vcf.gz"
+bcftools filter "$work_dir/ips.filter.vcf.gz" -e 'MAF[0] < 0.05' -Oz -o "$work_dir/ips.filter.m05.vcf.gz"
 
-# Extract indels and save their positions
-bcftools view -v indels $wd/ips_merged.vcf.gz | bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\n' > "$wd/ips_indels.tsv"
-
-# Make a bed file of genes to extract genic SNPs
+# Make a bed file of genes to extract genic and intergenic SNPs
 awk 'OFS="\t"{ if ($3=="gene") print $1, $4, $5 }' "$reference/$gff" > "$reference/$bed"
 
 # Intersect gene coordinates with VCF file to distinguish genic and intergenic SNPs
 # Create header of vcf file since bedtools omits it
-bcftools view -h "$wd/ips.filter.m05.vcf.gz" | tee "$wd/genic_variants.vcf" "$wd/intergenic_variants.vcf" > /dev/null
+bcftools view -h "$work_dir/ips.filter.m05.vcf.gz" | tee "$work_dir/genic_variants.vcf" "$work_dir/intergenic_variants.vcf" > /dev/null
 
 #Extract genic SNPs
-bedtools intersect -a "$wd/ips.filter.m05.vcf.gz" -b "$reference/$bed" -u >> "$wd/genic_variants.vcf"
-bgzip "$wd/genic_variants.vcf"
+bedtools intersect -a "$work_dir/ips.filter.m05.vcf.gz" -b "$reference/$bed" -u >> "$work_dir/genic_variants.vcf"
+bgzip "$work_dir/genic_variants.vcf"
 
 # Extract intergenic SNPs
-bedtools intersect -a "$wd/ips.filter.m05.vcf.gz" -b "$reference/$bed" -v >> "$wd/intergenic_variants.vcf"
-bgzip  "$wd/intergenic_variants.vcf"
+bedtools intersect -a "$work_dir/ips.filter.m05.vcf.gz" -b "$reference/$bed" -v >> "$work_dir/intergenic_variants.vcf"
+bgzip  "$work_dir/intergenic_variants.vcf"
 
-# Create header of readcounts table
-bcftools view -h "$wd/ips.filter.m05.vcf.gz" |tail -n1 | cut -f-2,4,5,10- | \
-  tee "$wd/genic_readcounts.tsv" "$wd/intergenic_readcounts.tsv" > /dev/null
+# Create header for read counts table
+bcftools view -h "$work_dir/ips.filter.m05.vcf.gz" |tail -n1 | cut -f-2,4,5,10- | \
+  tee "$work_dir/genic_readcounts.tsv" "$work_dir/intergenic_readcounts.tsv" > /dev/null
 
-# Get table of read counts of genic SNPs
-bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t[%AD\t]\n' "$wd/genic_variants.vcf.gz" | \
-  sed 's/\t$//' >> "$wd/genic_readcounts.tsv"
+# Get table of read counts for genic SNPs
+bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t[%AD\t]\n' "$work_dir/genic_variants.vcf.gz" | \
+  sed 's/\t$//' >> "$work_dir/genic_readcounts.tsv"
 
-# Get table of read counts of intergenic SNPs
-bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t[%AD\t]\n' "$wd/intergenic_variants.vcf.gz" | \
-  sed 's/\t$//' >> "$wd/intergenic_readcounts.tsv"
+# Get table of read counts for intergenic SNPs
+bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t[%AD\t]\n' "$work_dir/intergenic_variants.vcf.gz" | \
+  sed 's/\t$//' >> "$work_dir/intergenic_readcounts.tsv"
