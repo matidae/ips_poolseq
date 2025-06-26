47c7a967d5d17f5bd7b7571923d716044db251b2
Matias <matidae@gmail.com>
2025-06-26 16:23:24 +0200

    upload

--- 04_varcalls/04_calculate_m_and_z.py
+++ 04_varcalls/04_calculate_m_and_z.py
@@ -1,54 +1,52 @@
 #!/usr/bin/env python3
 
-#----------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # Calculates read depth (m) and Fisher-Ford transformation (z) for allele frequencies
 # Removes SNPs with depth values of outliers >1.5 median and <0.5 median
-# Run: ./04_calculate_m_and_z.py
-# In: tables of statistics calculated by ./02_get_SNPs_depth.py
-# Out: tsv file with m and z values for each SNP
-#----------------------------------------------------------------------
-
+#
+# Input: 
+#   - genic_depth_stats.tsv: summary statistics (mean, median, quantiles, IQR, whiskers) 
+#   - genic_readcounts.tsv: file with CHR, POS, REF, ALT, ADs* for all samples
+# Output: 
+#   - genic_m_and_z.tsv: tsv file with m and z values for each SNP
+#-------------------------------------------------------------------------------
 from math import sqrt, asin
+from utils import parse_counts, load_depth_threshold
 
-min_depth = 5508 # 1/2 the median
-max_depth = 16524 # 1.5 the median
+work_dir = "../../results/04_varcalls"
 
-wd = "../../results/04_varcalls"
+# Input files
+depth_stats_in = f"{work_dir}/genic_depth_stats.tsv"
+readcounts_in = f"{work_dir}/genic_readcounts.tsv"
+# Output files
+m_and_z_out = f"{work_dir}/genic_m_and_z.tsv"
 
-def parse_counts(count_str):    
-    ref, alt = map(int, count_str.split(','))
-    return ref, alt
+min_depth, max_depth = load_depth_threshold()
 
-# Calculates z Fisher transformation
+# Calculates Fisher-Ford arcsine square root transformation of allele frequencies
 def calculate_z_score(ref, alt):    
     m = ref + alt
     if m == 0:
         return 0, 'NA', 'NA'
     p = ref / m  # Calculates p as ref/(ref+alt)
-    z = 2 * asin(sqrt(p)) # Fisher transform of p
+    z = 2 * asin(sqrt(p)) # Fisher-Ford transform of p
     return m, f"{p:.4f}", f"{z:.4f}"
+def main():
+    with open(readcounts_in, 'r') as readcounts_fh, open(m_and_z_out, 'w') as m_and_z_fh:
+        header = next(readcounts_fh)
+        m_and_z_fh.write(header)
+        for line in readcounts_fh:        
+            cols = line.strip().split('\t')        
+            # Parse sample read counts
+            sample_counts = [parse_counts(x) for x in cols[4:]]
+            total_depth = sum(ref + alt for ref, alt in sample_counts)
+            # Filter by SNPs position depth sum
+            if min_depth <= total_depth <= max_depth:
+                m_and_z_fh.write('\t'.join(cols[:4]))
+                for ref, alt in sample_counts:
+                    m, p, z = calculate_z_score(ref, alt)
+                    m_and_z_fh.write(f'\t{m},{z}')
+                m_and_z_fh.write('\n')
 
-with open(f"{wd}/genic_readcounts.tsv", 'r') as infile, open(f"{wd}/genic_m_and_z.tsv", 'w') as out:
-    for line in infile:
-        cols = line.strip().split('\t')
-        # Write header
-        if cols[0] == "#CHROM":
-            out.write(line)
-            continue
-        # Parse sample read counts
-        sample_counts = [parse_counts(x) for x in cols[4:]]
-        total_depth = sum(ref + alt for ref, alt in sample_counts)
-
-        # Filter by SNPs position depth sum
-        if min_depth <= total_depth <= max_depth:
-            out.write('\t'.join(cols[:4]))
-            for ref, alt in sample_counts:
-                m, p, z = calculate_z_score(ref, alt)
-                out.write(f'\t{m},{z}')
-            out.write('\n')
-
-
-        
-
-
-
+if __name__ == "__main__":
+    main()
---
+++ 04_varcalls/05_pair_replicates_index.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+#-------------------------------------------------------------------------------
+# Creates a file with the column index of sample replicates
+#
+# Input: 
+#   - genic_m_and_z.tsv: table of m, z values
+# Output: 
+#   - paired_samples.tsv : indexes of paired samples
+#   - paired_samples.no_replicate.tsv : samples without replicates
+#-------------------------------------------------------------------------------
+import csv
+
+work_dir = "../../results/04_varcalls"
+
+# Input files
+m_and_z_in = f"{work_dir}/genic_m_and_z.tsv"
+
+# Output files
+paired_out = f"{work_dir}/samples_paired.tsv"
+not_paired_out = f"{work_dir}/samples_no_replicates.tsv"
+
+
+# Generate the file paired_samples.txt
+def paired_samples(m_and_z_in, paired_out, norep):
+    sample_dict = {}
+    xpositions = {} # maps sample to column idx of replicates
+    # Open genic_m_and_z to get the header
+    with open(m_and_z_in, "r") as m_and_z_fh, open(paired_out, "w") as paired_fh, \
+        open(not_paired_out, "w") as not_paired_fh:
+        reader = csv.reader(m_and_z_fh, delimiter="\t")
+        header = next(reader)
+        for idx, sample in enumerate(header[4:], start=4):  # start in col 4:
+            location, season_rep, year = sample.split("_")
+            season = season_rep[0]  # E or L
+            rep = season_rep[1]     # a or b
+            key = (location, season, year)
+            if key not in sample_dict:
+                    sample_dict[key] = {}
+            sample_dict[key][rep] = idx             
+        # Write paired samples to file
+        for (location, season, year), reps in sorted(sample_dict.items()):
+            if "a" in reps and "b" in reps:
+                name = f"{location}_{season}_{year}"
+                paired_fh.write(f"{name}\t{reps['a']}\t{reps['b']}\n")
+                xpositions[name] = [int(reps['a']), int(reps['b'])]
+            else:
+                # Write non paired samples to file
+                not_paired_fh.write(f"{location}_{season}_{year}: {reps}\n")
+    return xpositions
+
+def main():
+    paired_samples(m_and_z_in, paired_out, not_paired_out)
+
+if __name__ == "__main__":
+    main()
