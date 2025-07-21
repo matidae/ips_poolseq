d50868b553fccd9d023771573b9be9c0edfd10e2
Matias <matidae@gmail.com>
2025-07-21 16:58:36 +0200

    missing gt stats and plot

--- 04_varcalls/02_filter_stats.sh
+++ 04_varcalls/02_filter_stats.sh
@@ -5,24 +5,21 @@
 #
 # Input:
 #   - ips_merged.vcf: vcf file for all samples
-#
 # Output: 
-#   - stats_filter.tsv: file with number and proportion of SNPs per sample filtered by quality and genotype
+#   - missing_GT_stats.tsv: file with number and proportion of SNPs per sample with missing genotype
 #-------------------------------------------------------------------------------
 
 work_dir="../../results/04_varcalls"
 
-total_snps=$(bcftools view "$work_dir/ips_merged.vcf.gz" -m2 -M2 -v snps | grep -v '^#' | wc -l)
-
-bcftools view "$work_dir/ips_merged.vcf.gz" -m2 -M2 -v snps -i 'INFO/MQ<20 || QUAL<20' \
-  | bcftools query -f '[%SAMPLE\t%GT\n]' | cut -f1 | sort | uniq -c \
-  | sort -k1,1nr | awk '{print $1"\t"$2}' | sort -k2 \
-  > "$work_dir/qual_filter.tsv"
+total_snps=$(bcftools view "$work_dir/ips_merged.vcf.gz" -m2 -M2 -v snps -e 'INFO/MQ<20 || QUAL<20' \
+| bcftools filter -e 'MAF[0] < 0.05' | grep -v '^#' | wc -l)
 
 bcftools view "$work_dir/ips_merged.vcf.gz" -m2 -M2 -v snps -e 'INFO/MQ<20 || QUAL<20' \
-  | bcftools query -f '[%SAMPLE\t%GT\n]' | awk '$2 == "./."' | cut -f1 | sort | uniq -c \
-  | sort -k1,1nr | awk '{print $1"\t"$2}' | sort -k2 \
-  > "$work_dir/genotype_filter.tsv"
+| bcftools filter -e 'MAF[0] < 0.05' | bcftools query -f '[%SAMPLE\t%GT\n]' \
+| awk '$2 == "./."' | cut -f1 | sort | uniq -c | awk '{print $1"\t"$2}' | sort -k2 \
+> "$work_dir/missing_GT.tsv"
 
-paste "$work_dir/qual_filter.tsv" "$work_dir/genotype_filter.tsv" \
-| awk -v total="$total_snps" '{print $2, $1, $3, $1/total, $3/total}' > "$work_dir/stats_filter.tsv"
+awk -v total="$total_snps" '{printf "%s %s %.4f\n", $2, $1, $1/total}' "$work_dir/missing_GT.tsv" \
+> "$work_dir/missing_GT_stats.tsv"
+
+rm "$work_dir/missing_GT.tsv"
