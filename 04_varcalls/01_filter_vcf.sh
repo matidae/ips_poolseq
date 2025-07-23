164fbd0f431d36a6226ee133a428c2bdbce137ea
Matias <matidae@gmail.com>
2025-07-23 03:02:06 +0200

    add new script to filtering

--- 04_varcalls/01_filter_vcf.sh
+++ 04_varcalls/01_filter_vcf.sh
@@ -22,29 +22,32 @@ gff="Ips_typograpgus_LG16corrected.liftoff.gff"
 bed="Ips_typograpgus_LG16corrected.liftoff.genes.bed"
 
 # Filter VCF: -v snps (only snps), -m2 -M2 (biallelic) -e 'GT="./."' (exclude missing GT)
-bcftools view "$work_dir/ips_merged.vcf.gz" -m2 -M2 -v snps -e 'GT="./." || INFO/MQ<20 || QUAL<20' \
-  -Oz -o "$work_dir/ips.filter.vcf.gz" 
+bcftools view "$work_dir/ips_merged.2.vcf.gz" -m2 -M2 -v snps -e 'INFO/MQ<20 || QUAL<20' \
+  -Oz -o "$work_dir/ips.biallelic_q20_m20.vcf.gz" 
 
-# Exclude all SNPs where MAF < 0.05
-bcftools filter "$work_dir/ips.filter.vcf.gz" -e 'MAF[0] < 0.05' -Oz -o "$work_dir/ips.filter.m05.vcf.gz"
+# Exclude all SNPs where min(p) <= (1 - min_MAF) and max(p) >= min_MAF
+python3 01aux_filter_MAF.py
 
+#Exclude SNPs with missing genotype GT ./.
+bcftools view "$work_dir/ips.biallelic_q20_m20.maf05.vcf.gz" -e 'GT="./."' \
+-Oz -o "$work_dir/ips.biallelic_q20_m20.maf05.gt.vcf.gz" 
 # Make a bed file of genes to extract genic and intergenic SNPs
 awk 'OFS="\t"{ if ($3=="gene") print $1, $4, $5 }' "$reference/$gff" > "$reference/$bed"
 
 # Intersect gene coordinates with VCF file to distinguish genic and intergenic SNPs
 # Create header of vcf file since bedtools omits it
-bcftools view -h "$work_dir/ips.filter.m05.vcf.gz" | tee "$work_dir/genic_variants.vcf" "$work_dir/intergenic_variants.vcf" > /dev/null
+bcftools view -h "$work_dir/ips.biallelic_q20_m20.maf05.gt.vcf.gz" | tee "$work_dir/genic_variants.vcf" "$work_dir/intergenic_variants.vcf" > /dev/null
 
 #Extract genic SNPs
-bedtools intersect -a "$work_dir/ips.filter.m05.vcf.gz" -b "$reference/$bed" -u >> "$work_dir/genic_variants.vcf"
+bedtools intersect -a "$work_dir/ips.biallelic_q20_m20.maf05.gt.vcf.gz" -b "$reference/$bed" -u >> "$work_dir/genic_variants.vcf"
 bgzip "$work_dir/genic_variants.vcf"
 
 # Extract intergenic SNPs
-bedtools intersect -a "$work_dir/ips.filter.m05.vcf.gz" -b "$reference/$bed" -v >> "$work_dir/intergenic_variants.vcf"
+bedtools intersect -a "$work_dir/ips.biallelic_q20_m20.maf05.gt.vcf.gz" -b "$reference/$bed" -v >> "$work_dir/intergenic_variants.vcf"
 bgzip  "$work_dir/intergenic_variants.vcf"
 
 # Create header for read counts table
-bcftools view -h "$work_dir/ips.filter.m05.vcf.gz" |tail -n1 | cut -f-2,4,5,10- | \
+bcftools view -h "$work_dir/ips.biallelic_q20_m20.maf05.gt.vcf.gz" |tail -n1 | cut -f-2,4,5,10- | \
   tee "$work_dir/genic_readcounts.tsv" "$work_dir/intergenic_readcounts.tsv" > /dev/null
 
 # Get table of read counts for genic SNPs
