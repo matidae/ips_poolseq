c0b40d13914d85f8f5d9982461bdbd691406e1e6
Matias <matidae@gmail.com>
2025-07-25 02:21:27 +0200

    added excluded records

--- 06_SNPs_stats/00_calculate_m_and_z.py
+++ 06_SNPs_stats/00_calculate_m_and_z.py
@@ -8,7 +8,8 @@
 #   - genic_depth_stats.tsv: summary statistics (mean, median, quantiles, IQR, whiskers) 
 #   - genic_readcounts.tsv: file with CHR, POS, REF, ALT, ADs* for all samples
 # Output: 
-#   - genic_m_and_z.tsv: tsv file with m and z values for each SNP
+#   - genic_m_and_z.tsv: tsv file with m and z values for each SNP where min_depth <= total_depth <= max_depth
+#   - genic_m_and_z.excluded.tsv: tsv file with m and z values that where previous condition does not apply
 #-------------------------------------------------------------------------------
 import os
 from math import sqrt, asin
@@ -22,6 +23,7 @@ depth_stats_in = f"{work_dir}/genic_depth_stats.tsv"
 readcounts_in = f"{work_dir}/genic_readcounts.tsv"
 # Output files
 m_and_z_out = f"{out_dir}/genic_m_and_z.tsv"
+m_and_z_excluded_out = f"{out_dir}/genic_m_and_z.excluded.tsv"
 
 min_depth, max_depth = load_depth_threshold()
 
@@ -33,24 +35,31 @@ def calculate_z_score(ref, alt):
     p = ref / m  # Calculates p as ref/(ref+alt)
     z = 2 * asin(sqrt(p)) # Fisher-Ford transform of p
     return m, f"{p:.4f}", f"{z:.4f}"
+
 def main():
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-    with open(readcounts_in, 'r') as readcounts_fh, open(m_and_z_out, 'w') as m_and_z_fh:
+    with open(readcounts_in, 'r') as readcounts_fh, open(m_and_z_out, 'w') as m_and_z_fh, open(m_and_z_excluded_out, 'w') as m_and_z_excluded_fh:
         header = next(readcounts_fh)
         m_and_z_fh.write(header)
         for line in readcounts_fh:        
             cols = line.strip().split('\t')        
             # Parse sample read counts
-            sample_counts = [parse_counts(x) for x in cols[4:]]
-            total_depth = sum(ref + alt for ref, alt in sample_counts)
-            # Filter by SNPs position depth sum
+            sample_counts = [parse_counts(x) for x in cols[4:]]            
+            total_depth = sum(ref + alt for ref, alt in sample_counts)           
+            # Filter by SNPs position depth sum           
             if min_depth <= total_depth <= max_depth:
                 m_and_z_fh.write('\t'.join(cols[:4]))
                 for ref, alt in sample_counts:
                     m, p, z = calculate_z_score(ref, alt)
                     m_and_z_fh.write(f'\t{m},{z}')
                 m_and_z_fh.write('\n')
+            else:
+                m_and_z_excluded_fh.write('\t'.join(cols[:4]))                
+                for ref, alt in sample_counts:
+                    m, p, z = calculate_z_score(ref, alt)
+                    m_and_z_excluded_fh.write(f'\t{m},{z}')
+                m_and_z_excluded_fh.write('\n')
 
 if __name__ == "__main__":
     main()
