3ab9cc387aeeba60db02bb7b83b2f5a773c6ab26
Matias <matidae@gmail.com>
2025-08-02 01:44:19 +0200

    chisq scripts checked

--- 06_SNPs_stats/03_chisq_reps_div.py
+++ 06_SNPs_stats/03_chisq_reps_div.py
@@ -5,7 +5,7 @@
 # Compute chi-square statistic and p-value based on null variance (p-values < 0.01 are unreliable). 
 #
 # Inputs:
-#   - nv.genic_m_and_z: null variance estimates per sample
+#   - null_variance_summary.tsv: null variance estimates per sample
 #   - samples_paired.tsv: sample names and replicate column indices
 #   - genic_m_and_z.tsv: allele frequencies and counts per SNP
 #
@@ -13,11 +13,9 @@
 #   - snpdev.m_and_z.tsv: SNP-level coverage, comparisons, chi-square stat, and p-value
 #----------------------------------------------------------------------
 
-
-from math import sqrt
+from math import sqrt, pi
 from scipy.stats import chi2
 from utils import load_paired_samples
-from math import pi
 import pandas as pd
 
 work_dir = "../../results/06_SNPs_stats"
@@ -29,20 +27,20 @@ snpdev_out = f"{work_dir}/snpdev_m_and_z.tsv"
 cov_bin_out = f"{work_dir}/snp_depth_div_bin.tsv"
 
 # Parameters
-mincalls = 20  # Minimum number of SNPs per bulk
-min2 = 50 # Lowest count to include a sample
+#mincalls = 20  # Minimum number of SNPs per bulk
+min_depth = 50 # Lowest count to include a sample
 
 def load_null_variance(null_var_in):    
     df = pd.read_csv(null_var_in, sep='\t')
     null_var = dict(zip(df.iloc[:, 0], df.iloc[:, 5]))    
     return null_var
 
-def aggregate_SNPs_by_coverage_bin(stats, dcat, p_value):
-    if dcat not in stats:
-        stats[dcat] = [0, 0]
-    stats[dcat][0] += 1
+def aggregate_SNPs_by_coverage_bin(coverage_bin_stats, dcat, p_value):
+    if dcat not in coverage_bin_stats:
+        coverage_bin_stats[dcat] = [0, 0]
+    coverage_bin_stats[dcat][0] += 1
     if p_value < 0.01:
-        stats[dcat][1] += 1    
+        coverage_bin_stats[dcat][1] += 1    
 
 def write_SNP_stats(snpdev_out, cols_mz, m_total, snpstats, p_value, is_valid):
     if is_valid:        
@@ -57,7 +55,7 @@ def main():
     with open(m_and_z_in, "r") as m_and_z_fh, open(snpdev_out, "w") as snpdev_fh:
         snpdev_fh.write("CHROM\tPOS\tDepth\tNcomp\tChi2\tpval\n")
         next(m_and_z_fh)    
-        stats={}    
+        coverage_bin_stats={}    
 
         for line in m_and_z_fh:
             cols = line.strip().split('\t')       
@@ -70,10 +68,11 @@ def main():
                 m_a = int(rep_a[0])
                 rep_b = cols[paired_samples[sample][1]].split(",")
                 m_b = int(rep_b[0])
+                # Get total replicates depth
                 m_total += (m_a + m_b)
 
-                #If both replicate pass minimum read depth threshold, then calculate variance
-                if m_a>=min2 and m_b>=min2:
+                #If both replicate pass minimum read depth threshold, then calculate replicate variance
+                if m_a >= min_depth and m_b >= min_depth:
                     z_a = float(rep_a[1])
                     z_b = float(rep_b[1])
                     if (z_a + z_b) > 0 and (z_a + z_b) < 2 * pi: #sum of z values should be between 0 and 2 Pi
@@ -87,22 +86,22 @@ def main():
             p_value = 1.0 
             if snpstats[0]>0:  # If there were valid comparisons
                 if snpstats[1]>0:
-                    p_value=1.0-chi2.cdf(snpstats[1], snpstats[0])  # Chi-squared p-value
+                    p_value=1.0 - chi2.cdf(snpstats[1], snpstats[0])  # Chi-squared p-value
                 else:
                     p_value=1.0  # No divergence            
                 # Write SNP-level statistics
                 write_SNP_stats(snpdev_fh, cols, m_total, snpstats, p_value, is_valid=True)
                 # Aggregate statistics by coverage bin
-                aggregate_SNPs_by_coverage_bin(stats, dcat, p_value)             
+                aggregate_SNPs_by_coverage_bin(coverage_bin_stats, dcat, p_value)             
             else:  # No valid comparisons
                 write_SNP_stats(snpdev_fh, cols, m_total, snpstats, p_value, is_valid=False)
 
     with open(cov_bin_out, "w") as cov_bin_fh:
-        cov_bin_fh.write("depth_start\tSNP_count\tpval_lt_0.01\n")
-        for dcat in sorted(stats):
+        cov_bin_fh.write("depth_start\tSNP_count\tprop_pval_lt_0.01\n")
+        for dcat in sorted(coverage_bin_stats):
             coverage_start = dcat * 500
-            num_snps = stats[dcat][0]
-            prop = stats[dcat][1] / num_snps
+            num_snps = coverage_bin_stats[dcat][0]
+            prop = coverage_bin_stats[dcat][1] / num_snps
             cov_bin_fh.write(f"{coverage_start}\t{num_snps}\t{prop:.4f}\n")
 
 if __name__ == "__main__":
--- 06_SNPs_stats/03b_plot_chisq_results.py
+++ 06_SNPs_stats/03b_plot_chisq_results.py
@@ -5,7 +5,6 @@
 #
 # Inputs:
 #   - snpdev.m_and_z.tsv: SNP-level coverage, chi-square statistics, and p-values generated from replicate divergence testing.
-#
 # Outputs:
 #   - pval_histogram.png: Histogram plot of p-values.
 #----------------------------------------------------------------------
@@ -17,7 +16,11 @@ import numpy as np
 
 
 work_dir = "../../results/06_SNPs_stats"
+# Input files
 input_file = f"{work_dir}/snpdev_m_and_z.tsv"
+# Output files
+pval_histogram_plot = f"{work_dir}/pval_histogram.png"
+pval_vs_depth_plot = f"{work_dir}/pval_vs_depth.png"
 
 color= "#009688"
 plt.style.use("ggplot")
@@ -25,7 +28,7 @@ plt.style.use("ggplot")
 def load_data():
     return pd.read_csv(input_file, sep='\t')
 
-def plot_pval_histogram(df):
+def plot_pval_histogram(df, pval_histogram_plot):
     plt.figure(figsize=(8, 4))
     plt.hist(df['pval'], bins=100, color=color, edgecolor='white')
     plt.axvline(0.01, color='#8B0000', linestyle="--", linewidth=1.5 ,label="p = 0.01")
@@ -35,11 +38,11 @@ def plot_pval_histogram(df):
     plt.xlabel("p-value")
     plt.ylabel("SNPs count")
     plt.tight_layout()
-    plt.savefig(f"{work_dir}/pval_histogram.png")
+    plt.savefig(pval_histogram_plot)
     plt.close()
 
 
-def plot_depth_vs_pval(df):
+def plot_depth_vs_pval(df, depth_pval_plot):
     # Avoid log(0) by replacing zero p-values with the smallest positive float
     df["pval"] = df["pval"].replace(0, np.nextafter(0, 1))
     # Calculate -log10(pval)
@@ -62,13 +65,13 @@ def plot_depth_vs_pval(df):
     plt.ylabel("-log10(p-value)")
     plt.title("Chi2 p-value vs total depth of SNPs")
     plt.tight_layout()
-    plt.savefig(f"{work_dir}/pval_vs_depth.png", dpi=300)
+    plt.savefig(pval_vs_depth_plot, dpi=300)
     plt.close()
 
 def main():
     df = load_data()
-    plot_pval_histogram(df)
-    plot_depth_vs_pval(df)
+    plot_pval_histogram(df, pval_histogram_plot)
+    plot_depth_vs_pval(df, pval_vs_depth_plot)
 
 if __name__ == "__main__":
     main()
