152ee5e0b0a6d4fb843d51d84b80408cbdc61d11
Matias <matidae@gmail.com>
2025-10-02 15:51:19 +0200

    save dz to file for plotting

--- 06_SNPs_stats/06_Ne_estimation.py
+++ 06_SNPs_stats/06_Ne_estimation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 #----------------------------------------------------------------------
-# Estimates Ne 
+# Estimates effective population size (Ne). Reads the z-transformed AF across years
+# Calculates the changes in AF as dz between first and last year of each population
+# It corrects for sampling error and estimates Ne using both IQR and mean-square 
 #
 # Inputs:
-#   - Z.by.year.{prefix}.tsv - Z values per year
+#   - Z.by.year.{prefix}.tsv - Z values per year per population
 # Output:
-#   - Z.by.year.{prefix}.tsv - Z values per year
-#   - dz.max_interval.{prefix}.tsv - diff in Z values in consecutive year intervals 
+#   - Ne_estimates.tsv - Ne estimates for each population
 #----------------------------------------------------------------------
 
 from math import sqrt, asin
@@ -20,7 +21,7 @@ work_dir = "../../results/06_SNPs_stats"
 # Z.by.year.{prefix}.tsv - Z values per year (loaded dynamically)
 
 # Output files
-# Ne results file
+ne_out = f"{work_dir}/Ne_estimates.tsv" # Ne results file
 
 minMAF = 0.05
 z_low = 2.0*asin(sqrt(minMAF))
@@ -43,7 +44,8 @@ def compute_dz_variance(prefix):
     mean_evar =0
     mean_dz2 = 0    
     z_by_interval_in = f"{work_dir}/z_by_year.{prefix}.tsv"
-    with open(z_by_interval_in, 'r') as z_by_interval_fh:
+    dz_out = f"{work_dir}/dz_max.{prefix}.tsv"
+    with open(z_by_interval_in, 'r') as z_by_interval_fh, open(dz_out, "w") as dz_fh:
         next(z_by_interval_fh)
         n_row = 0
         for row in z_by_interval_fh:
@@ -61,6 +63,8 @@ def compute_dz_variance(prefix):
                     dz = z_first - z_last                    
                     if n_row%2:
                         dz = -dz
+                    # Save dz values of each population
+                    dz_fh.write(f"{dz:.5f}\n")
                     # Get first and last year SE values and estimate variance
                     se_first = float(cols[4].split(",")[1])
                     se_last = float(cols[-1].split(",")[1])
@@ -74,40 +78,36 @@ def compute_quantiles(dzlist):
     dz_values = np.array([dz for dz, _ in dzlist])
     q25 = np.percentile(dz_values, 25)
     q75 = np.percentile(dz_values, 75)
-    IQR = q75 - q25
-    return(IQR)
+    iqr = q75 - q25
+    return(iqr)
 
 def main():    
     paired_samples = load_paired_samples()
     prefixes = sorted(list({ "_".join(k.split("_")[:-1]) for k in paired_samples.keys() }))
-    samples = get_samples_years(prefixes)    
-    for key, value in samples.items():
-        prefix = key 
-        dzlist, mean_dz2, mean_evar = compute_dz_variance(prefix)
+    samples = get_samples_years(prefixes)
+    with open(ne_out, "w") as ne_fh:
+         # Write header 
+        ne_fh.write("\t".join(["prefix", "start", "end", "t", "SNPs",
+            "mean_dz2", "mean_evar", "IQR", "Ne_IQR", "Ne_dz2"]) + "\n")
+        for key, value in samples.items():
+            prefix = key 
+            dzlist, mean_dz2, mean_evar = compute_dz_variance(prefix)
 
-        nx = len(dzlist)
-        if nx > 0:                        
-            IQR = compute_quantiles(dzlist)
-            Q25 = -IQR / 2
-            start_year = value[0]
-            last_year = value[1]
-            t = int(value[1]) - int(value[0])
-            print(prefix, start_year, last_year)
-            print(f"IQR={IQR:.4f}, Q25={Q25:.4f}")            
-            print(f"SNPs included: {nx}")
-            mean_evar = mean_evar/float(nx) 
-            mean_dz2 = mean_dz2/float(nx)
-            print(f"cases: {nx}, mean dz2={mean_dz2:.4f}, Estimation var={mean_evar:.4f}")
-            
-            # Estimate Ne
-            ne_iqr = t/(2* ((IQR/1.34896)**2.0 - mean_evar))
-            ne_dz2 = t/(2* (mean_dz2 - mean_evar))
-            print(f"IQR Ne={ne_iqr:.2f}")
-            print(f"Ez2 Ne={ne_dz2:.2f}")            
-        else:
-            print(f"No SNPs passed filters for {prefix}")
-        
-        print("-"*30)
+            nx = len(dzlist)
+            if nx > 0:                        
+                iqr = compute_quantiles(dzlist)
+                q25 = -iqr / 2
+                start_year = value[0]
+                last_year = value[1]
+                t = int(value[1]) - int(value[0])                
+                mean_evar = mean_evar/float(nx) 
+                mean_dz2 = mean_dz2/float(nx)                
+                # Estimate Ne
+                ne_iqr = t/(2* ((iqr/1.34896)**2.0 - mean_evar))
+                ne_dz2 = t/(2* (mean_dz2 - mean_evar))                
+                # Save Ne to table                
+                ne_fh.write("\t".join(map(str,[prefix, start_year, last_year, t, nx,
+                f"{mean_dz2:.4f}", f"{mean_evar:.4f}", f"{iqr:.4f}", int(ne_iqr), int(ne_dz2)])) + "\n")            
 
 if __name__ == '__main__':
     main()
