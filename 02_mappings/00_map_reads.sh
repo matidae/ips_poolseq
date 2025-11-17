66d790104bd54f857ddb28c516bf4767ee746e5a
Matias <matidae@gmail.com>
2025-11-17 16:31:34 +0100

    reqwrite map_reads.sh to accept multiple runs

--- 02_mappings/00_map_reads.sh
+++ 02_mappings/00_map_reads.sh
@@ -1,86 +1,69 @@
 #!/usr/bin/env bash
 
-#----------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Mapping reads with BWA, sorting, merging and indexing with sambamba
-# Run: ./00_map_reads.sh
-# In: qc reads and genome
-# Out: alignment sort.bam files
-#----------------------------------------------------------------------
+#
+# Input: 
+#   - list of QC reads and genome for mapping
+# Output: 
+#   - merged and sorted alignment as {prefix}.sort.bam files
+#------------------------------------------------------------------------------
 
 
 while read -r prefix; do
     files=($(grep "$prefix" ./data/01_proc_reads/filelist))
-    numfiles=$(grep -c "$prefix" ./data/01_proc_reads/filelist)    
-    
-    read1=$(echo ${files[0]} | cut -f6 -d'/' | sed 's/.fq/.qc.fq/')
-    read2=$(echo ${files[1]} | cut -f6 -d'/' | sed 's/.fq/.qc.fq/')
+    numfiles=${#files[@]}
 
-    if [ "$numfiles" -eq 2 ]; then
+
+    for ((i=0; i<numfiles; i+=2)); do    
+        r1_base=$(echo ${files[$i]} | cut -f6 -d'/' | sed 's/.fq/.qc.fq/')
+        r2_base=$(echo ${files[$i+1]} | cut -f6 -d'/' | sed 's/.fq/.qc.fq/')
+
+
+        r1="./data/01_proc_reads/$prefix/$r1_base"
+        r2="./data/01_proc_reads/$prefix/$r2_base"
+
+        pair_id=$((i/2 + 1))
+
+        sam="./data/02_mappings/${prefix}.${pair_id}.sam"
+        bam="./data/02_mappings/${prefix}.${pair_id}.bam"
+        sorted="./data/02_mappings/${prefix}.sort.${pair_id}.bam"
+   
         #Run bwa for the first set of PE reads
         bwa-mem2 mem \
         -t 40 \
-        -R "@RG\tID:$prefix\tSM:$prefix" \
-        -o ./data/02_mappings/"$prefix".sam \
+        -R "@RG\tID:${prefix}\tSM:${prefix}" \
+        -o "$sam" \
         ./data/02_mappings/index/Ips_typograpgus_LG16corrected.final.bwa_index \
-        ./data/01_proc_reads/"$prefix"/"$read1" ./data/01_proc_reads/"$prefix"/"$read2" \
-        2>./data/02_mappings/"$prefix".bwa.log
-        
-        # Sam files to bam format
-        sambamba view -q -t 40 -S -f bam -o ./data/02_mappings/"$prefix".bam ./data/02_mappings/"$prefix".sam
-        
-        # Sort bam
-        sambamba sort -q -t 40 -o ./data/02_mappings/"$prefix".sort.bam ./data/02_mappings/"$prefix".bam
-        
-        # Index bam file
-        sambamba index -q -t 40 ./data/02_mappings/"$prefix".sort.bam
-
-        # Delete sam and bam files not sorted
-        rm ./data/02_mappings/"$prefix".sam
-        rm ./data/02_mappings/"$prefix".bam
-    
-    elif [ "$numfiles" -eq 4 ]; then
-        read3=$(echo ${files[2]} | cut -f6 -d'/' | sed 's/.fq/.qc.fq/')
-        read4=$(echo ${files[3]} | cut -f6 -d'/' | sed 's/.fq/.qc.fq/')
-        
-        #Run bwa for the first set of PE reads
-        bwa-mem2 mem \
-        -t 40 \
-        -R "@RG\tID:$prefix\tSM:$prefix" \
-        -o ./data/02_mappings/"$prefix".1.sam \
-        ./data/02_mappings/index/Ips_typograpgus_LG16corrected.final.bwa_index \
-        ./data/01_proc_reads/"$prefix"/"$read1" ./data/01_proc_reads/"$prefix"/"$read2" \
-        2>./data/02_mappings/"$prefix".bwa.log
-
-        #Run bwa for the second set of PE reads
-        bwa-mem2 mem \
-        -t 40 \
-        -R "@RG\tID:$prefix\tSM:$prefix" \
-        -o ./data/02_mappings/"$prefix".2.sam \
-        ./data/02_mappings/index/Ips_typograpgus_LG16corrected.final.bwa_index \
-        ./data/01_proc_reads/"$prefix"/"$read3" ./data/01_proc_reads/"$prefix"/"$read4" \
+        "$r1" "$r2" \
         2>>./data/02_mappings/"$prefix".bwa.log
-
+        
         # Sam files to bam format
-        sambamba view -q -t 40 -S -f bam -o ./data/02_mappings/"$prefix".1.bam ./data/02_mappings/"$prefix".1.sam
-        sambamba view -q -t 40 -S -f bam -o ./data/02_mappings/"$prefix".2.bam ./data/02_mappings/"$prefix".2.sam
-        
+        sambamba view -q -t 40 -S -f bam -o "$bam" "$sam"        
         # Sort bam
-        sambamba sort -q -t 40 -o ./data/02_mappings/"$prefix".sort.1.bam ./data/02_mappings/"$prefix".1.bam
-        sambamba sort -q -t 40 -o ./data/02_mappings/"$prefix".sort.2.bam ./data/02_mappings/"$prefix".2.bam
-        
-        # Merge both sorted bam files
-        sambamba merge -q -t 40 ./data/02_mappings/"$prefix".sort.bam ./data/02_mappings/"$prefix".sort.1.bam ./data/02_mappings/"$prefix".sort.2.bam
-        
-         # Index bam
-        sambamba index -q -t 40 ./data/02_mappings/"$prefix".sort.bam
+        sambamba sort -q -t 40 -o "$sorted" "$bam"
+        #Array of sorted bams
+        sorted_bams+=("$sorted")
+                
+        # Delete non sorted sam and bam files
+        rm "$sam" "$bam"
+    done
 
-        # Delete sam, bam, sort and index files before merging
-        rm ./data/02_mappings/"$prefix".1.sam ./data/02_mappings/"$prefix".2.sam
-        rm ./data/02_mappings/"$prefix".1.bam ./data/02_mappings/"$prefix".2.bam         
-        rm ./data/02_mappings/"$prefix".sort.1.bam ./data/02_mappings/"$prefix".sort.1.bam.bai 
-        rm ./data/02_mappings/"$prefix".sort.2.bam ./data/02_mappings/"$prefix".sort.2.bam.bai 
-        
+    merged_bams="./data/02_mappings/${prefix}.sort.bam"
 
-    fi 
+    # Merge if more than one sorted BAM
+    if (( ${#sorted_bams[@]} > 1 )); then
+        sambamba merge -q -t 40 "$merged_bams" "${sorted_bams[@]}"
+    else
+        cp "${sorted_bams[0]}" "$merged_bams"
+    fi
+
+    # Index bam file
+    sambamba index -q -t 40 "$merged_bam"
+
+    # Delete sam, bam, sort and index files before merging
+    for s in "${sorted_bams[@]}"; do
+        rm "$s" "$s.bai"
+    done      
 
 done < data/01_proc_reads/prefixes
