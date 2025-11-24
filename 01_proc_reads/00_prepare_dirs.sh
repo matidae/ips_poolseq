93336bb2267e445a975325747b35a1d2b3bc5ee9
Matias <matidae@gmail.com>
2025-11-24 17:33:31 +0100

    rewrite initial script for metadata and dir structure

--- 01_proc_reads/00_prepare_dirs.sh
+++ 01_proc_reads/00_prepare_dirs.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
 
 #----------------------------------------------------------------------
-# Generates files  with prefixes and paths to samples reads and creates
-# the directory structure for downstream analysis
+# Generates metadata files with prefixes and paths to reads
+# Also creates the directory structure for downstream analysis
 #
+# Input: 
+#   - reads in fq.gz format located in $reads_path
 # Output: 
 #    - prefixes: file with the prefixes of all samples 
 #    - filelist: list of all fastq files 
+#    - $out_path/$prefix (directory created for each sample)
 #----------------------------------------------------------------------
 
-reads_path="../../data/00_raw_reads/X204SC25022243*/01.RawData/*/*.fq.gz"
-out_path="./data/01_proc_reads"
+reads_path="../data/00_raw_reads"
+out_path="../data/01_proc_reads"
 
-#Create a file with the list of all the prefixes that will be used
-for i in $reads_path; do 
-    prefix=$(basename "$(dirname "$i")")
-    printf "%s\n" "$prefix"    
-done | sort | uniq > "$out_path/prefixes"
+#Create out directory if it doesn' t exist
+mkdir -p "$out_path"
 
-#Create a directory for each dataset
+#Create a file with a list of all the reads paths
+find "$reads_path/" -type f -name "*.fq.gz" > "$out_path/filelist"
+
+prefixes=($(awk -F'/' '{print $(NF-1)}' "$out_path/filelist" | sort -Vu))
+
+#Create a file with the list of all the sample prefixes 
+printf "%s\n" "${prefixes[@]}" > "$out_path/prefixes"
+
+#Create a directory to process the reads of each sample
 while read -r prefix; do 
-    mkdir -p "$out_path"/$prefix
+   mkdir -p "$out_path/$prefix"
 done < "$out_path/prefixes"
-
-#Create a list of all original files in the dataset
-for i in $reads_path; do 
-    printf "%s\n" "$i"
-done > "$out_path/filelist"
