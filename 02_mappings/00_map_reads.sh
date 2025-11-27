6490c67847254d529b074202b5fb3e907b23d0a7
Matias <matidae@gmail.com>
2025-11-27 16:48:08 +0100

    min changes, threads as param

--- 02_mappings/00_map_reads.sh
+++ 02_mappings/00_map_reads.sh
@@ -4,44 +4,55 @@
 # Mapping reads with BWA, sorting, merging and indexing with sambamba
 #
 # Input: 
-#   - list of QC reads and genome for mapping
+#   - list of processed FASTQ files 
+#   - reference genome
 # Output: 
-#   - merged and sorted alignment as {prefix}.sort.bam files
+#   - alignments as sorted and merged BAM file: $prefix.sort.bam
 #------------------------------------------------------------------------------
 
+in_dir="../data/01_proc_reads"
+out_dir="../data/02_mappings"
+
+$threads=40
+# Input
+prefixes="$in_dir/prefixes"
+filelist="$in_dir/filelist"
+
+mkdir -p "$out_dir/index"
 
 while read -r prefix; do
-    files=($(grep "$prefix" ./data/01_proc_reads/filelist))
+    sorted_bams=()
+    files=($(grep "/$prefix/" "$filelist"))
     numfiles=${#files[@]}
 
 
     for ((i=0; i<numfiles; i+=2)); do    
-        r1_base=$(echo ${files[$i]} | cut -f6 -d'/' | sed 's/.fq/.qc.fq/')
-        r2_base=$(echo ${files[$i+1]} | cut -f6 -d'/' | sed 's/.fq/.qc.fq/')
+        r1_base=$(basename "${files[$i]}" | sed 's/.fq/.qc.fq/')
+        r2_base=$(basename "${files[$i+1]}" | sed 's/.fq/.qc.fq/')
 
 
-        r1="./data/01_proc_reads/$prefix/$r1_base"
-        r2="./data/01_proc_reads/$prefix/$r2_base"
+        r1="$in_dir/$prefix/$r1_base"
+        r2="$in_dir/$prefix/$r2_base"
 
         pair_id=$((i/2 + 1))
 
-        sam="./data/02_mappings/${prefix}.${pair_id}.sam"
-        bam="./data/02_mappings/${prefix}.${pair_id}.bam"
-        sorted="./data/02_mappings/${prefix}.sort.${pair_id}.bam"
+        sam="$out_dir/$prefix.$pair_id.sam"
+        bam="$out_dir/$prefix.$pair_id.bam"
+        sorted="$out_dir/$prefix.sort.$pair_id.bam"
    
         #Run bwa for the first set of PE reads
         bwa-mem2 mem \
-        -t 40 \
+        -t "$threads" \
         -R "@RG\tID:${prefix}\tSM:${prefix}" \
         -o "$sam" \
-        ./data/02_mappings/index/Ips_typograpgus_LG16corrected.final.bwa_index \
+        "$out_dir/index/Ips_typograpgus_LG16corrected.final.bwa_index" \
         "$r1" "$r2" \
-        2>>./data/02_mappings/"$prefix".bwa.log
+        2>> "$out_dir/$prefix.bwa.log"
         
         # Sam files to bam format
-        sambamba view -q -t 40 -S -f bam -o "$bam" "$sam"        
+        sambamba view -q -t "$threads" -S -f bam -o "$bam" "$sam"        
         # Sort bam
-        sambamba sort -q -t 40 -o "$sorted" "$bam"
+        sambamba sort -q -t "$threads" -o "$sorted" "$bam"
         #Array of sorted bams
         sorted_bams+=("$sorted")
                 
@@ -49,21 +60,21 @@ while read -r prefix; do
         rm "$sam" "$bam"
     done
 
-    merged_bams="./data/02_mappings/${prefix}.sort.bam"
+    merged_bams="$out_dir/$prefix.sort.bam"
 
     # Merge if more than one sorted BAM
     if (( ${#sorted_bams[@]} > 1 )); then
-        sambamba merge -q -t 40 "$merged_bams" "${sorted_bams[@]}"
+        sambamba merge -q -t "$threads" "$merged_bams" "${sorted_bams[@]}"
     else
         cp "${sorted_bams[0]}" "$merged_bams"
     fi
 
     # Index bam file
-    sambamba index -q -t 40 "$merged_bam"
+    sambamba index -q -t "$threads" "$merged_bams"
 
-    # Delete sam, bam, sort and index files before merging
+    # Delete sam, bam, sort and index files after merging
     for s in "${sorted_bams[@]}"; do
         rm "$s" "$s.bai"
     done      
 
-done < data/01_proc_reads/prefixes
+done < "$prefixes"
