9249a681c897430e5881f18d998983c8f1c064b0
Matias <matidae@gmail.com>
2025-12-03 15:53:27 +0100

    min format changes

--- 03_dedup/02_mosdepth_plots.py
+++ 03_dedup/02_mosdepth_plots.py
@@ -1,23 +1,25 @@
 #----------------------------------------------------------------------
 # Make mosdepth plots per genomic window for the html map & dedup report.
-# In: *.mosdepth.regions.bed.gz
-# Ouput lineplots of depth average per bin.
+# Input: 
+#   - *.mosdepth.regions.bed.gz
+# Ouput:
+#   - depth average per bin plotsto be used downstram in the summary report
 #----------------------------------------------------------------------
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import glob
+import os
 
-# Set working directory and load data
-wd = "../../results/03_dedup/"
+# Working directory 
+out_dir = "../results/03_dedup/"
 
 def plot(filepath, outplot):
     df = pd.read_csv(filepath, sep='\t', header=None,
-                    names=["chrom", "start", "end", "depth"])
+            names=["chrom", "start", "end", "depth"])
 
     # Sort by contig and start, keep only chromo
-    df = df[df["chrom"].str.startswith("LG")]    
+    df = df[df["chrom"].str.startswith("LG")]
     df.sort_values(by=["chrom", "start"], inplace=True)
 
     # Get  contig lengths
@@ -34,7 +36,6 @@ def plot(filepath, outplot):
     # Add cumulative start positions
     df["cum_start"] = df.apply(lambda row: row["start"] + offsets[row["chrom"]], axis=1)
 
-    
     ylim_max = 500
     fig, ax = plt.subplots(figsize=(12, 5))
 
@@ -42,27 +43,27 @@ def plot(filepath, outplot):
     ax.set_xlim(0, df["cum_start"].max())
     ax.set_ylim(0, ylim_max)
     ax.set_xlabel("Chromosomes")
-    ax.set_ylabel("Depth")
-    title = filepath.split("/")[4].split(".")[0]
+    ax.set_ylabel("Depth")    
+    title = os.path.basename(filepath).split(".")[0]
     ax.set_title(title + " - avg depth per 500k windows")
 
-    # 2. Alternate background colors
+    # Alternate background colors
     for i, contig in enumerate(contigs_sorted):
         start = offsets[contig]
         end = start + contig_lengths[contig]
         if i % 2 == 0:
             ax.axvspan(start, end, color=(0.9, 0.9, 0.9, 0.5))
 
-    # 3. Plot depth lines
+    # Plot depth lines
     for contig in contigs_sorted:
         sub_df = df[df["chrom"] == contig]
         ax.plot(sub_df["cum_start"], sub_df["depth"], color="royalblue", linewidth=2)
 
-    #Add contig boundary lines
+    # Add contig boundary lines
     for contig in contigs_sorted:
         ax.axvline(x=offsets[contig], color="gray", linestyle="dashed", linewidth=0.8)
 
-    #Add vertical line at end of last contig
+    # Add vertical line at end of last contig
     last_contig = contigs_sorted[-1]
     end_last_contig = offsets[last_contig] + contig_lengths[last_contig]
     ax.axvline(x=end_last_contig, color="gray", linestyle="dashed", linewidth=0.8)
@@ -80,12 +81,15 @@ def plot(filepath, outplot):
 
     # Final layout
     plt.style.use('default')
-    plt.tight_layout()    
+    plt.tight_layout()
     plt.savefig(outplot, dpi=300)
-    #plt.show()
 
-#Start call here
-files = glob.glob("../../results/03_dedup/mosdepth/LAAU_Ea_2023.mosdepth.regions.bed.gz")
-for i in files:    
-    outplot = i.replace("mosdepth.regions.bed.gz", "depth.500K_bins.png").replace("mosdepth","depth")
-    plot(i, outplot)
+def main():
+    files = glob.glob(out_dir + "depth_metrics/*.mosdepth.regions.bed.gz")
+    for i in files:
+        outplot = i.replace("mosdepth.regions.bed.gz", "depth.500K_bins.png")
+        plot(i, outplot)
+
+if __name__ == "__main__":
+    main()
+
