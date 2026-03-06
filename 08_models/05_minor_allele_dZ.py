343c61b4b78c2e1297fd743b9c488dd588517077
Matias <matidae@gmail.com>
2026-03-06 15:51:46 +0100

    renaming sync score

--- 08_models/05_minor_allele_dZ.py
+++ 08_models/05_minor_allele_dZ.py
@@ -4,7 +4,7 @@
 # Characterizes temporal AFs dynamics of SNPs under a fluctuating model;
 # SNPs are minor-allele polarized, and dz is computed  between consecutive intervals. 
 # Interval types are inferred from the sign of mean dz  and used to compute a 
-# "greenscore" per SNP measuring if it follows the population-avg fluctuation.
+# "sync_score" per SNP measuring if it follows the population-avg fluctuation.
 #
 # Inputs:
 #    thinned_file    : tsv file with one SNP per gene 
@@ -14,33 +14,33 @@
 #   - _deltaZ_per_snp.tsv      : dz per SNP per interval
 #   - _mean_deltaZ_ttests.tsv  : mean dz per interval with t-tests
 #   - _correlations.tsv        : Pearson correlation matrix between intervals
-#   - _greenscores.tsv         : greenscore and rank per SNP
-#   - _interval_types.tsv      : interval types (green/yellow) inferred from mean dz
+#   - _sync_scores.tsv         : sync_score and rank per SNP
+#   - _interval_types.tsv      : interval types (up/down) inferred from mean dz
 #----------------------------------------------------------------------
 
 import math
 import glob
 from scipy.stats import pearsonr, ttest_1samp
-import csv, os, sys
+import csv, os
 
 work_dir = "../results/08_models"
-thinned_snps_dir = "../results/08_models/snp_to_gene"
+thinned_snps_dir = "../results/08_models/s2_thinning"
 out_dir = "../results/08_models/analysis"
 
 # Input files
-thinned_snps_in = sorted(glob.glob(f"{thinned_snps_dir}/tests_*_fluctuating.thinned.tsv"))
+thinned_snps_in = sorted(glob.glob(f"{thinned_snps_dir}/tests.*.fluctuating.thinned.tsv"))
 
 # Output files
 #tests_fdr_out = [f.replace(".tsv", "_FDR.tsv") for f in tests_in]
 
-prefixes = ["_".join(os.path.basename(f).split("_")[1:3]) for f in thinned_snps_in]
+prefixes = [os.path.basename(f).split(".")[1] for f in thinned_snps_in]
 
 #Read thinned SNP file, returning a set
-def read_thinned_file(thinned_file):    
+def read_thinned_file(thinned_file):
     rsnps = set()
     with open(thinned_file, "r") as f:
         for line in f:
-            cols = line.strip().split("\t")            
+            cols = line.strip().split("\t")
             rsnps.add(cols[1] + "_" + cols[2]) # chromosome_position as key
     return rsnps
 
@@ -94,13 +94,20 @@ def read_z_file(z_file, rsnps):
 
     return dznext, dz_per_snp, years, snp_zbars
 
+#Extracts year from timepoint label: 'WFIN_E_2022' -> '2022'
+def simplify_interval(y1, y2):
+   
+    year1 = str(y1).split("_")[-1]
+    year2 = str(y2).split("_")[-1]
+    return f"{year1}-{year2}"
+
 # Computes mean dz, t-tests, correlations, and saves output files.
-def analyze_and_save(dznext, dz_per_snp, years, prefix, snp_zbars, greenscores, interval_types):
+def analyze_and_save(dznext, dz_per_snp, years, prefix, snp_zbars, sync_scores, interval_types):
     # - Save dz per SNP per interval
     dz_file = prefix + "_deltaZ_per_snp.tsv"
     with open(dz_file, "w", newline="") as f:
         writer = csv.writer(f, delimiter="\t")
-        writer.writerow(["SNP"] + [f"{years[i]}_to_{years[i+1]}" for i in range(len(years)-1)])
+        writer.writerow(["SNP"] + [simplify_interval(years[i], years[i+1]) for i in range(len(years)-1)])
         for snp, dzlist in dz_per_snp.items():
             writer.writerow([snp] + dzlist)
 
@@ -112,14 +119,14 @@ def analyze_and_save(dznext, dz_per_snp, years, prefix, snp_zbars, greenscores, 
         for i, year in enumerate(years[:-1]):
             mean_dz = sum(dznext[year]) / len(dznext[year])
             tval, pval = ttest_1samp(dznext[year], 0.0)
-            label = f"{year}_to_{years[i+1]}"
+            label = simplify_interval(year, years[i+1])
             writer.writerow([label, mean_dz, tval, pval])
 
     # - Correlation matrix
     corr_file = prefix + "_correlations.tsv"
     with open(corr_file, "w", newline="") as f:
         writer = csv.writer(f, delimiter="\t")
-        intervals = [f"{years[i]}_to_{years[i+1]}" for i in range(len(years)-1)]
+        intervals = [simplify_interval(years[i], years[i+1]) for i in range(len(years)-1)]
         writer.writerow([""] + intervals)
         #writer.writerow([""] + years[:-1])
         n = len(years) - 1
@@ -134,17 +141,17 @@ def analyze_and_save(dznext, dz_per_snp, years, prefix, snp_zbars, greenscores, 
         for i, interval in enumerate(intervals):
             writer.writerow([interval] + corr_matrix[i])
 
-    # - Green score per SNP
-    greenscore_file = prefix + "_greenscores.tsv"
-    with open(greenscore_file, "w", newline="") as f:
+    # - Sync score per SNP
+    sync_score_file = prefix + "_sync_scores.tsv"
+    with open(sync_score_file, "w", newline="") as f:
         writer = csv.writer(f, delimiter="\t")
-        writer.writerow(["SNP", "greenscore", "mean_z", "rank"])
-        sorted_snps = sorted(greenscores.items(), key=lambda x: x[1], reverse=True)
+        writer.writerow(["SNP", "sync_score", "mean_z", "rank"])
+        sorted_snps = sorted(sync_scores.items(), key=lambda x: x[1], reverse=True)
         for rank, (snp, gs) in enumerate(sorted_snps, start=1):
             zbar = snp_zbars[snp]  # mean_z stored during read_z_file
             writer.writerow([snp, round(gs, 6), round(zbar, 6), rank])
 
-    # Correlation greenscore vs mean allele frequency
+    # Correlation sync_score vs mean allele frequency
     gs_vals = [gs for _, gs in sorted_snps]
     zbar_vals = [snp_zbars[snp] for snp, _ in sorted_snps]
     r, p = pearsonr(gs_vals, zbar_vals)
@@ -156,36 +163,38 @@ def analyze_and_save(dznext, dz_per_snp, years, prefix, snp_zbars, greenscores, 
         writer.writerow(["interval", "type", "mean_dz"])
         for i, year in enumerate(years[:-1]):
             mean_dz = sum(dznext[year]) / len(dznext[year])
-            label = f"{year}_to_{years[i+1]}"
+            label = simplify_interval(year, years[i+1])
             writer.writerow([label, interval_types[i], round(mean_dz, 6)])
 
 # Infers interval types from mean dz, and scores SNPs by how consistently follow population-avg fluctuation.
-def compute_greenscores(dznext, dz_per_snp, years):
+def compute_sync_scores(dznext, dz_per_snp, years):
     # Infer interval types
     interval_types = []
     for year in years[:-1]:
         mean_dz = sum(dznext[year]) / len(dznext[year])
-        interval_types.append("g" if mean_dz > 0 else "y")
+        interval_types.append("up" if mean_dz > 0 else "down")
     # Score each SNP
-    greenscores = {}
+    sync_scores = {}
     for snp, dz_list in dz_per_snp.items():
-        greenscore = 0.0
+        sync_score = 0.0
         for j, dz in enumerate(dz_list):
-            greenscore += dz if interval_types[j] == "g" else -dz
-        greenscores[snp] = greenscore
-    return greenscores, interval_types
+            sync_score += dz if interval_types[j] == "up" else -dz
+        sync_scores[snp] = sync_score
+    return sync_scores, interval_types
 
 def main():
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
     for pre in prefixes:
-        thinned_file = f"{thinned_snps_dir}/tests_{pre}_FDR_fluctuating.thinned.tsv"
-        output_prefix = f"{out_dir}/{pre}_fluctuating"
+        thinned_file = f"{thinned_snps_dir}/tests.{pre}.FDR.fluctuating.thinned.tsv"
+        print(thinned_file)
+        output_prefix = f"{out_dir}/{pre}.fluctuating"
+       ### output_prefix = f"{out_dir}/{pre}_fluctuating_nopolar"
         z_file = f"{work_dir}/z_year.{pre}.tsv"
         rsnps = read_thinned_file(thinned_file)
         dznext, dz_per_snp, years, snp_zbars = read_z_file(z_file, rsnps)
-        greenscores, interval_types = compute_greenscores(dznext, dz_per_snp, years)
-        analyze_and_save(dznext, dz_per_snp, years, output_prefix, snp_zbars, greenscores, interval_types)
+        sync_scores, interval_types = compute_sync_scores(dznext, dz_per_snp, years)
+        analyze_and_save(dznext, dz_per_snp, years, output_prefix, snp_zbars, sync_scores, interval_types)
 
 if __name__ == "__main__":
     main()
