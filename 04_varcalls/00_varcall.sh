f85b22f583d88648672650dce62151eb61b1fa5e
Matias <matidae@gmail.com>
2026-03-17 14:58:33 +0100

    minor refactor of variant calling script

--- 04_varcalls/00_varcall.sh
+++ 04_varcalls/00_varcall.sh
@@ -10,7 +10,7 @@
 # Output: 
 #   - ips_merged.vcf.gz : vcf file for all samples
 #-------------------------------------------------------------------------------
-
+set -euo pipefail
 eval "$(conda shell.bash hook)"
 conda activate extra
 
@@ -23,6 +23,7 @@ jobs=40
 genome="../data/reference"
 #List of samples to exclude from varcalling
 exclude_file="$work_dir/exclude_samples.txt"
+
 #List of dedup sorted bam files for analysis (optional removal of ones listed in exclude_file)
 if [[ -s "$exclude_file" ]]; then
     bam=$(find "$dedup_dir/" -maxdepth 1 -name "*.dedup.sort.bam" | grep -v -f "$exclude_file" | sort | tr '\n' ' ')
@@ -35,15 +36,16 @@ bedtools makewindows -g "$genome/Ips_typograpgus_LG16corrected.final.fasta.fai" 
     | awk '{print $1":"$2+1"-"$3}' > "$work_dir/regions_1M.txt"
 
 #Run bcftools mpileup and call in parallel taking genome chunks defined in regions_1M
-cat "$work_dir/regions_1M.txt" | parallel -j $jobs "
+parallel -j $jobs --halt soon,fail=1 "
     region={};
     chunk=\$(echo \$region | sed 's/[:-]/_/g');
-    bcftools mpileup -Ou -I -a FORMAT/AD --max-depth 5000 \
+    bcftools mpileup -Ou -I -a FORMAT/AD --max-depth 5000 -q 20 -Q 20 \
         -r \$region \
         -f \"$genome/Ips_typograpgus_LG16corrected.final.fasta\" \
         $bam |
     bcftools call -vmO v -o \"$work_dir/region_{#}_\${chunk}.vcf\"
-"
+" < "$work_dir/regions_1M.txt"
+
 # Compress and index each VCF file
 for i in "$work_dir"/*.vcf; do
     bgzip "$i"
@@ -54,7 +56,7 @@ done
 sorted_vcfs=$(ls "$work_dir"/region_*.vcf.gz | sort -V)
 
 # Concatenate the sorted VCF files
-bcftools concat -Oz -o "$work_dir/ips_merged.vcf.gz" $sorted_vcfs
+bcftools concat -Oz -o "$work_dir/ips_merged.vcf.gz" "$sorted_vcfs"
 
 # Index the merged VCF
 tabix -p vcf "$work_dir/ips_merged.vcf.gz"
