8384a2c63d80dc0fac4cba403d20445bc4c63c52
Matias <matidae@gmail.com>
2026-04-14 17:13:05 +0200

    fix: update z_mean calculation logic and  filtering criteria

--- 08_models/00_z_mean.py
+++ 08_models/00_z_mean.py
@@ -12,8 +12,9 @@
 import sys
 sys.path.append("./utils")
 import os
-from math import sqrt, asin
-from utils import load_paired_samples, load_null_variance_recalc
+from math import sqrt
+from utils import load_paired_samples, load_null_variance_recalc, \
+    MIN_READ_DEPTH, Z_LOW, Z_HIGH
 
 work_dir = "../results/08_models"
 input_dir = "../results/07_null_variance"
@@ -24,11 +25,6 @@ m_and_z_in = f"{input_dir}/genic_m_and_z.filter.tsv"
 # Output files
 #z_mean_out = f"{work_dir}/z_mean.{prefix}.tsv"
 
-#Filter rules
-minMAF = 0.05
-zlow = 2.0*asin(sqrt(minMAF))
-zhigh= 2.0*asin(sqrt(1.0-minMAF))
-min_depth = 100
 
 def calculate_z_mean(prefixes, paired_samples, null_var, m_and_z_in):
     season_order = {"E": 0, "L": 1}
@@ -63,19 +59,22 @@ def calculate_z_mean(prefixes, paired_samples, null_var, m_and_z_in):
                 z_mean_fh.write(row_start)
                 # Iterate over each group of replicates
                 for sample in ordered_samples:
+                    if sample not in null_var:
+                        z_mean_fh.write(f"\tNA,NO_VAR,0,0")
+                        continue
                     rep_a = cols[paired_samples[sample][0]].split(",")
                     m_a = int(rep_a[0])
                     rep_b = cols[paired_samples[sample][1]].split(",")
                     m_b = int(rep_b[0])
                     
-                    if m_a >= min_depth and m_b >= min_depth:
+                    if m_a >= MIN_READ_DEPTH and m_b >= MIN_READ_DEPTH:
                         z_a = float(rep_a[1])
                         z_b = float(rep_b[1])
                         z_mean=(z_a + z_b)/2.0
                         var = (null_var[sample] + 1.0/float(m_a) + 1.0/float(m_b))/4.0
                         std_error = sqrt(var)
                         # Write in z_mean.{pre}.tsv ... z_mean, SE, depth_rep_b, depth_rep_b
-                        if z_mean > zlow and z_mean < zhigh:
+                        if z_mean >= Z_LOW and z_mean <= Z_HIGH:
                             z_mean_fh.write(f"\t{z_mean:.4f},{std_error:.4f},{m_a},{m_b}")
                         else:
                             z_mean_fh.write(f"\tNA,Z_FIL,{m_a},{m_b}")
@@ -89,8 +88,6 @@ def main():
     null_var = load_null_variance_recalc()
     paired_samples = load_paired_samples()
     prefixes = sorted(list({ k.split("_")[0] for k in paired_samples.keys() }))
- 
- 
     calculate_z_mean(prefixes, paired_samples, null_var, m_and_z_in)
 
 if __name__ == '__main__':
