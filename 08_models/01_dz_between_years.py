3cb4b0c55fd7aaf52af2211731ecb2d47c6a17dd
Matias <matidae@gmail.com>
2026-04-22 16:47:37 +0200

    fix: streamline z and dz calculations by removing hardcoded values

--- 08_models/01_dz_between_years.py
+++ 08_models/01_dz_between_years.py
@@ -14,8 +14,9 @@
 import sys
 sys.path.append("./utils")
 import os
-from math import sqrt, asin
-from utils import load_paired_samples, load_null_variance_recalc
+from math import sqrt
+from utils import load_paired_samples, load_null_variance_recalc, \
+    MIN_READ_DEPTH, Z_LOW, Z_HIGH
 
 work_dir = "../results/08_models"
 input_dir = "../results/07_null_variance"
@@ -28,11 +29,6 @@ m_and_z_in = f"{input_dir}/genic_m_and_z.filter.tsv"     # Genic m and z data in
 #dz_by_year_out = f"{work_dir}/z_interval.{prefix}.tsv"  # z diff in z values in consecutive year intervals 
 
 def calculate_z_dz(prefixes, paired_samples, null_var, m_and_z_in):
-    minMAF = 0.05
-    zlow = 2.0*asin(sqrt(minMAF))
-    zhigh= 2.0*asin(sqrt(1.0-minMAF))
-    min_depth = 20
-
     for pre in prefixes:
         paired_samples_set= {}
         paired_samples_year_list= []
@@ -48,12 +44,11 @@ def calculate_z_dz(prefixes, paired_samples, null_var, m_and_z_in):
         dz_by_interval_out = f"{work_dir}/dz_interval.{pre}.tsv"
         
         with open(m_and_z_in, "r") as m_and_z_fh, \
-            open(z_by_year_out, "w") as z_by_year_fh:
-            # Check if there are more than 1 year to compare            
+            open(z_by_year_out, "w") as z_by_year_fh, \
+            open(dz_by_interval_out, "w") as dz_by_interval_fh:
+            # Check if there are more than 1 year to compare
             if num_years == 1:
                 continue
-            dz_by_interval_fh = open(dz_by_interval_out, "w")
-                        
             # Write header (sample-specific year field for z values)
             z_by_year_fh.write("CHROM\tPOS\tREF\tALT")
             for sample in paired_samples_set:
@@ -62,15 +57,15 @@ def calculate_z_dz(prefixes, paired_samples, null_var, m_and_z_in):
 
             # Write header (sample-specific year_interval field for dz values)
             dz_by_interval_fh.write("CHROM\tPOS\tREF\tALT")
-            for i in range(len(paired_samples_year_list) - 1):                                    
+            for i in range(len(paired_samples_year_list) - 1):
                 pre_year = paired_samples_year_list[i]
-                pos_year = paired_samples_year_list[i+1]                
-                dz_by_interval_fh.write(f"\t{pre}_{pre_year}-{pos_year}")            
+                pos_year = paired_samples_year_list[i+1]
+                dz_by_interval_fh.write(f"\t{pre}_{pre_year}-{pos_year}")
             dz_by_interval_fh.write("\n")            
 
             # Start analyzing m and z file
             next(m_and_z_fh)
-            for line in m_and_z_fh:                    
+            for line in m_and_z_fh:
                 cols = line.strip().split('\t')
                 row_start = "\t".join(cols[:4])
                 # Write in z_year.{pre}.tsv: chromo, coor, ref, alt ...
@@ -78,15 +73,22 @@ def calculate_z_dz(prefixes, paired_samples, null_var, m_and_z_in):
                 dz_by_interval_fh.write(row_start)
                 #Set last value of z to NA
                 last_z = ["NA","NA"]                
-                year_count = 0
+                is_first_year = True
 
                 # Iterate over each group of replicates
                 for sample in paired_samples_set:
+                    if sample not in null_var:
+                        z_by_year_fh.write("\tNA,NA,0,0")
+                        if not is_first_year:
+                            dz_by_interval_fh.write("\tNA,NA")
+                        is_first_year = False
+                        last_z = ["NA", "NA"]
+                        continue    
                     rep_a = cols[paired_samples[sample][0]].split(",")
                     m_a = int(rep_a[0])
                     rep_b = cols[paired_samples[sample][1]].split(",")
                     m_b = int(rep_b[0])
-                    if m_a >= min_depth and m_b >= min_depth:
+                    if m_a >= MIN_READ_DEPTH and m_b >= MIN_READ_DEPTH:
                         z_a = float(rep_a[1])
                         z_b = float(rep_b[1])
                         z_mean=(z_a + z_b)/2.0
@@ -94,16 +96,16 @@ def calculate_z_dz(prefixes, paired_samples, null_var, m_and_z_in):
                         std_error = sqrt(var)
 
                         # Write in z_year.{pre}.tsv ... z_mean, SE, depth_rep_b, depth_rep_b
-                        if z_mean > zlow and z_mean < zhigh:
+                        if z_mean >= Z_LOW and z_mean <= Z_HIGH:
                             z_by_year_fh.write(f"\t{z_mean:.6f},{std_error:.6f},{m_a},{m_b}")
                         else:
                             z_by_year_fh.write("\tNA,NA,0,0")
 
                         # Write in dz_interval.{pre}.tsv ... z_mean, SE, depth_rep_b, depth_rep_b
-                        if year_count > 0:
+                        if not is_first_year:
                             if last_z[0] != 'NA':
                                 z_mean_interval = (z_mean + last_z[0])/2.0
-                                if z_mean_interval > zlow and z_mean_interval < zhigh:
+                                if z_mean_interval >= Z_LOW and z_mean_interval <= Z_HIGH:
                                     dz2 = z_mean - last_z[0]
                                     evar = (null_var[sample] + 1.0/float(m_a) + 1.0/float(m_b))/4.0 + last_z[1]
                                     dz_by_interval_fh.write(f"\t{dz2:.6f},{evar:.6f}")
@@ -112,13 +114,16 @@ def calculate_z_dz(prefixes, paired_samples, null_var, m_and_z_in):
                             else:
                                 dz_by_interval_fh.write("\tNA,NA")
                         else:                            
-                            year_count+=1
-                        last_z = [z_mean, (null_var[sample] + 1.0/float(m_a) + 1.0/float(m_b))/4.0]
+                            is_first_year = False
+                        if z_mean >= Z_LOW and z_mean <= Z_HIGH:
+                            last_z = [z_mean, (null_var[sample] + 1.0/float(m_a) + 1.0/float(m_b))/4.0]
+                        else:
+                            last_z = ["NA", "NA"]
                     else:
                         z_by_year_fh.write("\tNA,NA,0,0")
-                        if year_count > 0:
+                        if not is_first_year:
                             dz_by_interval_fh.write("\tNA,NA")
-                        year_count+=1
+                        is_first_year = False
                         last_z = ["NA","NA"]
                 z_by_year_fh.write("\n")
                 dz_by_interval_fh.write("\n")
